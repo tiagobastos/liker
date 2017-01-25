a70fc4e3212e14a15c4af29af7ad1bc1f4b3cce2
Tiago Bastos <thbastos@gmail.com>
2017-01-25 00:29:54 +0000

    Post index endpoint

--- app/Post.php
+++ app/Post.php
@@ -13,4 +13,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeLatestFirst($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
 }
